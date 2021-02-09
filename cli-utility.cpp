dc9b69d0a520d0c2e7075f9786a95f9166e69b1c
JunYe1993 <d486250@gmail.com>
2021-02-09 17:49:22 +0800

    option->needs() sample code

--- cli-utility.cpp
+++ cli-utility.cpp
@@ -12,7 +12,7 @@ struct util_cmd_option
     FnPtr option_func;
 };
 
-class MCTP_Formatter : public CLI::Formatter 
+class myFormatter : public CLI::Formatter 
 {
     protected:
         std::string get_option_name(std::string str) const
@@ -25,7 +25,7 @@ class MCTP_Formatter : public CLI::Formatter
         }
 
     public:
-        MCTP_Formatter() : Formatter() {}
+        myFormatter() : Formatter() {}
         std::string make_option_opts(const CLI::Option * opt) const override 
         { 
             return get_option_name(opt->get_name()); 
@@ -59,7 +59,9 @@ option_init_raw_command(std::vector<uint8_t> *rawData, CLI::App* app, std::vecto
 static int
 option_get_version(CLI::Option* opt)
 {
-    std::cout << "Hello" << std::endl;
+    std::string arg;
+    opt->results(arg);
+    std::cout << arg << std::endl;
     return 0;
 }
 
@@ -83,25 +85,59 @@ option_init_get_version (std::string *strData, CLI::App* app, std::vector<struct
     return;
 }
 
+static int
+option_raw_command_w_flag (CLI::Option* opt) 
+{
+    // std::vector<std::string> arg;
+    // opt->results(arg);
+    // 
+    // for (const auto& data : arg)
+    //     std::cout << data << " ";
+    // std::cout << std::endl;
+
+    return 0;
+}
+
+static void 
+option_init_raw_command_w_flag (std::string *device, std::vector<uint8_t> *rawCommand, CLI::App* app, std::vector<struct util_cmd_option>* opts)
+{
+    CLI::Option *sub_option, *main_option;
+    std::string optionDescription = "Send raw command with flag.";
+    std::set<std::string> validate_flags = {"nic", "other"};
+
+    sub_option = app->add_option("-d", *device, optionDescription);
+    sub_option->check(CLI::IsMember(validate_flags));
+    
+    main_option = app->add_option("--raw2", *rawCommand, optionDescription);
+    main_option->needs(sub_option);
+    opts->push_back({main_option, option_raw_command_w_flag});
+    return;
+}
+
 int 
 main(int argc, char **argv) 
 {
-    CLI::App app("MCTP Utility");
+    CLI::App app("Utility");
     std::vector< struct util_cmd_option > opts;
     int ret = 0;
     
     // Setting "--help" message
-    auto fmt = std::make_shared<MCTP_Formatter>();
+    auto fmt = std::make_shared<myFormatter>();
     fmt->column_width(37);
-    app.failure_message(CLI::FailureMessage::help);
+    app.failure_message(CLI::FailureMessage::simple);
     app.formatter(fmt);
     
     // Option init
     std::vector<uint8_t> rawCommand;
     option_init_raw_command(&rawCommand, &app, &opts);
+
     std::string deviceName;
     option_init_get_version(&deviceName, &app, &opts);
 
+    std::string rawCommandsFlag;
+    std::vector<uint8_t> raw2Command;
+    option_init_raw_command_w_flag(&rawCommandsFlag, &raw2Command, &app, &opts);
+    
     // Parse and execute
     CLI11_PARSE(app, argc, argv);
     for (const auto& opt : opts)
